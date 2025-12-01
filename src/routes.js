1e516bccd0ed380d528eb3b074996819323e4a06
Guilherme Canina <161065800+GuilhermeCanina@users.noreply.github.com>
2025-11-30 22:15:28 -0300

    arrumando as coisas

--- src/routes.js
+++ src/routes.js
@@ -31,19 +31,19 @@ routes.get('/respostas/:usuarioId', autenticarToken, respostasController.relator
 routes.post('/usuarios', validateUser, usuariosController.createUser);
 routes.post('/login', validateUser, usuariosController.loginUser);
 
-routes.get('/usuarios', autenticarToken, isAdmin, usuariosController.getAllUsers); // ← apenas admin
+routes.get('/usuarios', autenticarToken, isAdmin, usuariosController.getAllUsers); 
 routes.get('/usuarios/:id', autenticarToken, usuariosController.getUserById);
 routes.put('/usuarios/:id', autenticarToken, usuariosController.updateUser);
-routes.delete('/usuarios/:id', autenticarToken, isAdmin, usuariosController.deleteUser); // ← apenas admin
-routes.patch('/usuarios/nome', autenticarToken, usuariosController.updateMyName); // ← atualizar nome do usuário~
-routes.patch('/usuarios/email', autenticarToken, usuariosController.updateMyEmail); // ← atualizar email do usuário
+routes.delete('/usuarios/:id', autenticarToken, isAdmin, usuariosController.deleteUser);
+routes.patch('/usuarios/nome', autenticarToken, usuariosController.updateMyName);
+routes.patch('/usuarios/email', autenticarToken, usuariosController.updateMyEmail); 
 
 routes.get('/me', autenticarToken, usuariosController.getMe);
-routes.delete('/me', autenticarToken, usuariosController.deleteMe); // ← excluir própria conta
+routes.delete('/me', autenticarToken, usuariosController.deleteMe); 
 
 // ROTAS DE MEDALHA
 routes.get('/usuarios/:id/medalhas', autenticarToken, usuariosController.getUserMedalhas);
-routes.post('/medalhas', autenticarToken, isAdmin, medalhasController.createMedalha); // ← apenas admin
+routes.post('/medalhas', autenticarToken, isAdmin, medalhasController.createMedalha);
 
 
 // ROTAS DE SESSÕES DE ESTUDO
